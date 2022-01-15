01c207b73aabd63efa3bdc8585e117d130a62411
Marcin Lewicki <lemarcin@gmail.com>
2022-01-15 11:06:53 -0500

    Work on project. Stage 2/7: Grader

--- Main.java
+++ Main.java
@@ -1,59 +1,43 @@
 package bullscows;
 
 import java.util.Arrays;
-
-
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
         int[] secret = new int[4];
         int[] answer = new int[4];
-        int[] input = new int[8];
-        int bulls;
-        int turn = 0;
-        input[0] = 1234;
-        input[1] = 5678;
-        input[2] = 9012;
-        input[3] = 9087;
-        input[4] = 1087;
-        input[5] = 9205;
-        input[6] = 9305;
-        input[7] = 9305;
-        for (int i = 0; i < secret.length; i++) {
-            secret[i] = input[7] % 10;
-            input[7] /= 10;
+        int bulls = 0;
+        int cows = 0;
+        int input = scanner.nextInt();
+        for (int i = answer.length -1; i > -1; i--) {
+            secret[i] = random.nextInt(10);
+            answer[i] = input % 10;
+            input /= 10;
         }
-        System.out.println("The secret code is prepared: ****.");
-        do {
-            bulls = 0;
-            int cows = 0;
-            System.out.printf("\nTurn %d. Answer:\n", turn + 1);
-            System.out.println(input[turn]);
-            for (int i = 0; i < answer.length; i++) {
-                answer[i] = input[turn] % 10;
-                input[turn] /= 10;
-                for (int j = secret.length - 1; j > -1; j--) {
-                    if (secret[j] == answer[i]) {
-                        cows++;
-                        if (i == j) {
-                            cows--;
-                            bulls++;
-                        }
+        for (int i = 0; i < secret.length; i++) {
+            for (int j = 0; j < secret.length; j++) {
+                if (answer[i] == secret[j]) {
+                    cows++;
+                    if (answer[i] == secret[i]) {
+                        cows--;
+                        bulls++;
                     }
+                    continue;
                 }
             }
-            turn++;
-            System.out.print("Grade: ");
-            if (cows == 0 && bulls == 0) System.out.print("None");
-            else if (cows == 1) System.out.print("1 cow");
-            else if (cows > 1) System.out.print(cows + "cows");
-            if (cows > 0 && bulls > 0) System.out.print(" and ");
-            if (bulls == 1) System.out.print("1 bull");
-            else if (bulls > 1) System.out.print(bulls + " bulls");
-            System.out.println(".");
-        } while (bulls < 4);
-        System.out.print("Congrats! The secret code is ");
+        }
+        System.out.print("Grade: ");
+        if (cows == 0 && bulls == 0) System.out.print("None");
+        if (bulls > 0) System.out.print(bulls + " bull(s)");
+        if (cows > 0 && bulls > 0) System.out.print(" and ");
+        if (cows > 0) System.out.print(cows + " cow(s)");
+        System.out.print(". ");
+        System.out.print("The secret code is ");
         System.out.print(Arrays.toString(secret).replaceAll("[\\[\\]\\,\\ ]", "") + ".");
     }
 }
