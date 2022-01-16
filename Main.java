fa9d729f7f6749c2f248bba0d799590c8ad8d1bd
Marcin Lewicki <lemarcin@gmail.com>
2022-01-16 12:46:09 -0500

    Work on project. Stage 3/7: Secret code

--- Main.java
+++ Main.java
@@ -1,43 +1,23 @@
 package bullscows;
 
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        int[] secret = new int[4];
-        int[] answer = new int[4];
-        int bulls = 0;
-        int cows = 0;
-        int input = scanner.nextInt();
-        for (int i = answer.length -1; i > -1; i--) {
-            secret[i] = random.nextInt(10);
-            answer[i] = input % 10;
-            input /= 10;
-        }
-        for (int i = 0; i < secret.length; i++) {
-            for (int j = 0; j < secret.length; j++) {
-                if (answer[i] == secret[j]) {
-                    cows++;
-                    if (answer[i] == secret[i]) {
-                        cows--;
-                        bulls++;
-                    }
-                    continue;
+        int length = scanner.nextInt();
+        if (length > 10) {
+            System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.", length);
+        } else {
+            String result = new String("");
+            do {
+                String pseudoRandom = String.valueOf((long) System.nanoTime()).replaceAll("0*$", "");
+                for (int i = pseudoRandom.length() - 1; i >= 0; i--) {
+                    if (result.indexOf(pseudoRandom.charAt(i)) < 0 ) { result += pseudoRandom.charAt(i); }
+                    if (result.length() == length) { break; }
                 }
-            }
+            } while (result.length() < length);
+            System.out.println(result);
         }
-        System.out.print("Grade: ");
-        if (cows == 0 && bulls == 0) System.out.print("None");
-        if (bulls > 0) System.out.print(bulls + " bull(s)");
-        if (cows > 0 && bulls > 0) System.out.print(" and ");
-        if (cows > 0) System.out.print(cows + " cow(s)");
-        System.out.print(". ");
-        System.out.print("The secret code is ");
-        System.out.print(Arrays.toString(secret).replaceAll("[\\[\\]\\,\\ ]", "") + ".");
     }
 }
